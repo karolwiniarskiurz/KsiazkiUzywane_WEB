3bf6fe418f1b09185555fff57d6a3744c944bd0c
Unknown <kwiniarski97@gmail.com>
2018-04-24 17:48:53 +0200

    clean angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
