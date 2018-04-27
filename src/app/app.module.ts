ce17d52b51523da754ad9b19a6094b0c8c806ed2
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-27 12:26:43 +0200

    routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {MatAutocompleteModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { SearchBoxComponent } from './searchbox/search-box.component';
-import { HomeComponent } from './home/home.component';
+import {SearchBoxComponent} from './searchbox/search-box.component';
+import {HomeComponent} from './home/home.component';
 import {RouterModule} from '@angular/router';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { PostFullComponent } from './searchbox/post-full/post-full.component';
-import { PostShortComponent } from './searchbox/post-short/post-short.component';
-import { AddPostFormComponent } from './add-post-form/add-post-form.component';
+import {SearchResultsComponent} from './search-results/search-results.component';
+import {PostFullComponent} from './searchbox/post-full/post-full.component';
+import {PostShortComponent} from './searchbox/post-short/post-short.component';
+import {AddPostFormComponent} from './add-post-form/add-post-form.component';
 
 
 @NgModule({
@@ -38,10 +38,12 @@ import { AddPostFormComponent } from './add-post-form/add-post-form.component';
     RouterModule.forRoot([
       {path: '', component: HomeComponent, pathMatch: 'full'},
       {path: 'add', component: AddPostFormComponent},
-      {path: 'search', component: SearchResultsComponent}
-      ])
+      {path: 'search/:phrase', component: SearchResultsComponent},
+      {path: 'post/:id', component: PostFullComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
